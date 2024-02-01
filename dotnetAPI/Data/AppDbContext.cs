77a522858ce49f85aa2dc3169f7e3be831cb5c32
Sam Mayer <sammayer0815@gmail.com>
2024-02-01 16:39:51 +0100

    removed wrong packages and installed correct ones.

---
+++ dotnetAPI/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+// Data/AppDbContext.cs
+
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Item> Items { get; set; }
+}
